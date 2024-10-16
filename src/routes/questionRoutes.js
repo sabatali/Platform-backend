70d524d32d08d0a41e21dfe8d43ef10298c67fdb
Sabat Ali <126473010+sabatali@users.noreply.github.com>
2024-10-16 14:33:19 +0500

    Update questionRoutes.js

--- src/routes/questionRoutes.js
+++ src/routes/questionRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addQuestion, getAllQuestions, getSingleQuestion } from '../controllers/questionControllers.js';
+import { addQuestion, getAllQuestions, getSingleQuestion, deleteQuestion } from '../controllers/questionControllers.js';
 import { searchBlos } from '../controllers/scraperControllers.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
 import { gemini } from '../controllers/geminiControllers.js';
@@ -9,6 +9,7 @@ const questionRoute = express.Router();
 questionRoute.post("/question", authMiddleware, addQuestion);
 questionRoute.get("/questions", getAllQuestions);
 questionRoute.get("/question/:id", getSingleQuestion);
+questionRoute.post("/question/:id", deleteQuestion);
 questionRoute.post("/resources", searchBlos);
 questionRoute.post("/aiapi", gemini);
 
