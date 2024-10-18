36ba6bf7cf51d8b56414efb1355a7af02a073268
Sabat Ali <126473010+sabatali@users.noreply.github.com>
2024-10-19 04:22:48 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
-# Assuming the JDK installer is in the root of your repository
 JDK_INSTALLER="jdk-22_windows-x64_bin.exe"
 
 # Path to install JDK
@@ -11,5 +10,5 @@ mkdir -p $INSTALL_PATH
 echo "Installing JDK..."
 cp $JDK_INSTALLER $INSTALL_PATH
 
-# Run the JDK installer (silent mode or customize based on your requirements)
-$INSTALL_PATH$JDK_INSTALLER /s
+# Run the JDK installer (silent mode)
+$INSTALL_PATH"jdk-22_windows-x64_bin.exe" /s
