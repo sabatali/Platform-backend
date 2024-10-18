9ecfd1ed8ae5bac778694c1a14055466250f50a1
Sabat Ali <126473010+sabatali@users.noreply.github.com>
2024-10-19 04:18:19 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,40 +1,15 @@
-#!/bin/bash
+# Assuming the JDK installer is in the root of your repository
+JDK_INSTALLER="jdk-22_windows-x64_bin.exe"
 
-# Specify JDK version and download URL
-jdk_version="17.0.8"
-jdk_download_url="https://download.java.net/java/GA/jdk${jdk_version}/binaries/openjdk-17.0.8_linux-x64_bin.tar.gz"  # Update this URL as needed
-jdk_install_dir="/opt/render/project/src/jdk"
+# Path to install JDK
+INSTALL_PATH="/opt/render/project/src/bin/jdk/"
 
-# Create installation directory
-mkdir -p "$jdk_install_dir"
+# Create the install directory
+mkdir -p $INSTALL_PATH
 
-# Download JDK
-echo "Downloading OpenJDK $jdk_version..."
-wget -q "$jdk_download_url" -O /tmp/openjdk.tar.gz
+# Install JDK
+echo "Installing JDK..."
+cp $JDK_INSTALLER $INSTALL_PATH
 
-# Check if download was successful
-if [ $? -eq 0 ]; then
-    echo "OpenJDK downloaded successfully."
-else
-    echo "Failed to download OpenJDK."
-    exit 1
-fi
-
-# Extract JDK
-echo "Extracting OpenJDK..."
-tar -xzf /tmp/openjdk.tar.gz -C "$jdk_install_dir" --strip-components=1
-
-# Set JAVA_HOME and update PATH
-echo "Setting JAVA_HOME and updating PATH..."
-export JAVA_HOME="$jdk_install_dir"
-export PATH="$JAVA_HOME/bin:$PATH"
-
-# Verify the installation
-if java -version; then
-    echo "OpenJDK installed successfully."
-else
-    echo "Failed to install OpenJDK."
-    exit 1
-fi
-
-echo "Build completed successfully!"
+# Run the JDK installer (silent mode or customize based on your requirements)
+$INSTALL_PATH$JDK_INSTALLER /s
