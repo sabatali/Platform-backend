5d78937f2021cc4e345b53848577c2167eacf4c6
sabatali <maliksabatali@gmail.com>
2024-10-19 22:26:47 +0500

    feedbacke

--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -1,6 +1,7 @@
 import { register, verifyEmail, userData, login, AdminMail, UpdateUser } from "../controllers/user-controllers.js";
 import express from 'express';
 import { authMiddleware } from "../middlewares/authMiddleware.js";
+import { FeedBack } from "../controllers/FeedBacke.js";
 
 const regRoute = express.Router();
 
@@ -9,6 +10,7 @@ regRoute.post("/verifyemail", verifyEmail);
 regRoute.post("/login", login)
 regRoute.post("/adminmail/:id", AdminMail)
 regRoute.get("/updateuser/:id", UpdateUser)
+regRoute.post("/feedbacke", FeedBack)
 regRoute.get("/userdata", authMiddleware, userData)
 
 
